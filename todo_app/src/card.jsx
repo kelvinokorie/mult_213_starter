5662bf7e47a257366143a786ed26999ba2fb7e44
kelvin okorie <okoriekelvin236@gmail.com>
2025-12-09 15:24:14 -0600

    i remove the image from the carding to make my carding look good

--- todo_app/src/card.jsx
+++ todo_app/src/card.jsx
@@ -1,9 +1,8 @@
 import './card.css';
 
-export function Card({ title, subtitle, content, image, backgroundColor, onDelete }) {
+export function Card({ title, subtitle, content, backgroundColor, onDelete }) {
   return (
     <div className="card" style={{ backgroundColor }}>
-      <img src={image} alt={title} className="card__image" />
       <div className="card__content">
         <h2>{title}</h2>
         <h3>{subtitle}</h3>
